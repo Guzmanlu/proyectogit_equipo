e11c0ce6c69c31e9cac60eb9b29cd93e75ab6255
Ethan Acosta <ethanlevacosta17@gmail.com>
2026-02-12 23:23:14 -0600

    Configuración Inicial en indexJS.js

--- JavaScript/indexJS.js
+++ JavaScript/indexJS.js
@@ -1,4 +1,5 @@
-window.addEventListener("load", () =>
-{
-    
+window.addEventListener("load", () => {
+    const audios = document.querySelectorAll('audio');
+    const mainHeader = document.querySelector('main header h2');
+    const canciones = Array.from(document.querySelectorAll('ol li a'), a => a.textContent);
 });
