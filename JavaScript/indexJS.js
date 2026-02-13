97985f2921f2f83d6602f64022635d19a311b9e3
Ethan Acosta <ethanlevacosta17@gmail.com>
2026-02-12 23:24:51 -0600

    Se Agrega un Sistema de Pausa Automática en indexJS.js

--- JavaScript/indexJS.js
+++ JavaScript/indexJS.js
@@ -2,4 +2,16 @@ window.addEventListener("load", () => {
     const audios = document.querySelectorAll('audio');
     const mainHeader = document.querySelector('main header h2');
     const canciones = Array.from(document.querySelectorAll('ol li a'), a => a.textContent);
+
+    audios.forEach((audio, index) => {
+        audio.addEventListener('play', () => {
+            audios.forEach((otroAudio, otroIndex) => {
+                if (otroIndex !== index) {
+                    otroAudio.pause();
+                }
+            });
+
+            mainHeader.textContent = `🎵 Ahora suena: ${canciones[index]} 🎵`;
+        });
+    });
 });
